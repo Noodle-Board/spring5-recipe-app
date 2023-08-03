61a308a88b605131cb5b246f73bc058e5f4dedce
= <=>
2023-08-03 08:12:40 -0400

    Creates POJOs

---
+++ src/main/java/guru/springframework/domain/Category.java
@@ -0,0 +1,12 @@
+package guru.springframework.domain;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by ze on 8/3/23
+ */
+@Entity
+public class Category {
+
+    private String categoryName;
+}
